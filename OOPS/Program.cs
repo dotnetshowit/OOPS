4ed225cfa3b7bb6be037097ea52cf1d85fb4ebfb
dotnetshowit <dotnetshowit@gmail.com>
2024-01-30 09:29:36 +0530

    Add project files.

---
+++ OOPS/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student();
+
+            student.StudentName;
+
+            student.StudentId = 1;
+
+            var studentID = student.StudentAge;
+
+
+            //student.Id = 0;
+            //student.SetId(-1);
+            //student.SetName("ShowIT");
+            //student.age = -100;
+
+            //Console.WriteLine(student.GetId());
+            //Console.WriteLine(student.GetName());
+            //Console.WriteLine(student.GetAge());
+
+            
+
+            Console.ReadLine();
+
+
+            // form values
+            var DTO = new SignUpDTO();
+
+            // Domain object
+            var studentToAdd = new Student();
+
+            //studentToAdd.age = DTO.Age;
+            //studentToAdd.Name = DTO.Name;
+
+            //context.saveChanges(studentToAdd);
+
+
+        }
+    }
+}
---
+++ OOPS/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class Student
+    {
+
+        private int _Id;
+        private string FirstName;
+        private string LastName;
+        private DateTime dateOfBirth;
+
+        //public int Student { get; set; }
+        public int StudentId { 
+            get
+            {
+                return this._Id;
+            }
+        }
+
+
+        public int StudentAge {
+            get
+            {
+                // Save today's date.
+                var today = DateTime.Today;
+
+                // Calculate the age.
+                var age = today.Year - dateOfBirth.Year;
+
+                // Go back to the year in which the person was born in case of a leap year
+                if (dateOfBirth.Date > today.AddYears(-age)) age--;
+                // using dateOfBirth
+
+                return age;
+            }
+        }
+
+
+        public Student()
+        {
+
+        }
+
+
+        //public void SetId(int id)
+        //{
+        //    if(id < 0)
+        //    {
+        //        throw new ArgumentException("Id cannot be less than 0");
+        //    }
+        //    this.Id = id;
+        //}
+
+        //public int GetId()
+        //{
+        //    return this.Id;
+        //}
+
+        //public int GetAge()
+        //{
+          
+        //}
+
+        
+        //public void SetName(string name)
+        //{
+        //    this.Name = name;
+        //}
+
+        public string GetName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+
+        // Variables
+
+
+        // Properties
+        public string StudentName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        //public int StudentAge { get; set; }
+    }
+}
